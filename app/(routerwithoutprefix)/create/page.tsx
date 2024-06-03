7b8b8be5bdd15b59c730f457c7d14032cf40fecd
Prasanna R <prasannnarajendran@gmail.com>
2024-06-03 11:01:02 +0530

    oauth

---
+++ app/(routerwithoutprefix)/create/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {}
+
+const page = () => {
+  return <div>
+    npw we call call this file directly after localhst:3000 since it has the  parantehisis in its parent folder.
+  </div>
+}
+
+export default page
---
+++ app/_thiscomponentwillnotcomesunderroute/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface Props {}
+
+const page = () => {
+  return <div>
+    <h1>This componenet will not be routed. since the naming convention has _ in prefix.</h1>
+  </div>
+}
+
+export default page
